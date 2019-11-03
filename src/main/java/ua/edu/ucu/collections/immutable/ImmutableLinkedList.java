74014f2c9cbf63b3a066d86b07751f895c33c5ab
smaxym <02smaxym@gmail.com>
2019-11-03 18:27:23 +0200

    Added all functions and tests to linked list

--- src/main/java/ua/edu/ucu/collections/immutable/ImmutableLinkedList.java
+++ src/main/java/ua/edu/ucu/collections/immutable/ImmutableLinkedList.java
@@ -20,39 +20,134 @@ public final class ImmutableLinkedList implements ImmutableList{
         this.size = instance.size();
     }
 
+    public void checkIndex(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= size()){
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
     public Node getRoot() {
         return root;
+    }
+
+    public void setRoot(Node e) {
+        this.root = e;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
     }
 
     public void incrementSize() {
         size += 1;
     }
 
-    @Override
-    public ImmutableList add(Object e) {
+    private ImmutableList addNode(Node n, int size) {
         ImmutableLinkedList linkedList = new ImmutableLinkedList(this);
+        if (size == 0) {
+            return linkedList;
+        }
         if (linkedList.getRoot() == null) {
-            linkedList = new ImmutableLinkedList(e);
+            linkedList = new ImmutableLinkedList(n.getValue());
             return linkedList;
         }
         Node currentTarget = linkedList.getRoot();
         for (int i = 1; i < linkedList.size(); i++) {
             currentTarget = currentTarget.getNext();
         }
-        currentTarget.setNext(new Node(e));
-        linkedList.incrementSize();
+        currentTarget.setNext(n);
+        for (int i = 0; i < size; ++i) {
+            linkedList.incrementSize();
+        }
+        return linkedList;
+    }
+
+    private ImmutableList addNode(Node n, int index, int size) throws IndexOutOfBoundsException {
+        checkIndex(index);
+        ImmutableLinkedList linkedList = new ImmutableLinkedList(this);
+        if (size == 0) {
+            return linkedList;
+        }
+        Node currentTarget, address;
+        currentTarget = linkedList.getRoot();
+        for (int i = 1; i < index; i++) {
+            currentTarget = currentTarget.getNext();
+        }
+        address = currentTarget.getNext();
+        currentTarget.setNext(n);
+        for (int i = 0; i < size; ++i) {
+            linkedList.incrementSize();
+            currentTarget = currentTarget.getNext();
+        }
+        currentTarget.setNext(address);
         return linkedList;
     }
 
     @Override
+    public ImmutableList add(Object e) {
+        return addNode(new Node(e), 1);
+    }
+
+    @Override
     public ImmutableList add(int index, Object e) throws IndexOutOfBoundsException {
-        if (index >= this.size()) {
-            throw new IndexOutOfBoundsException();
+        return addNode(new Node(e), index, 1);
+    }
+
+    private Node buildSequence(Object[] c) {
+        int len = c.length;
+        if (len == 0) {
+            return null;
         }
-        ImmutableLinkedList linkedList = new ImmutableLinkedList(this);
-        linkedList.incrementSize();
-        Node currentTarget = linkedList.getRoot();
+        Node root = new Node(c[0]);
+        Node currentTarget = root;
+        for (int i = 1; i < len; ++i) {
+            currentTarget.setNext(new Node(c[i]));
+            currentTarget = currentTarget.getNext();
+        }
+        return root;
+    }
+
+    @Override
+    public ImmutableList addAll(Object[] c) {
+        Node objSequence = buildSequence(c);
+        return addNode(objSequence, c.length);
+    }
+
+    @Override
+    public ImmutableList addAll(int index, Object[] c) {
+        Node objSequence = buildSequence(c);
+        return addNode(objSequence, index, c.length);
+    }
+
+    @Override
+    public Object get(int index) throws IndexOutOfBoundsException {
+        checkIndex(index);
+        Node currentTarget = getRoot();
         for (int i = 0; i < index; i++) {
+            currentTarget = currentTarget.getNext();
+        }
+        return currentTarget.getValue();
+    }
+
+    @Override
+    public ImmutableList remove(int index) {
+        checkIndex(index);
+        ImmutableLinkedList linkedList = new ImmutableLinkedList(this);
+        Node currentTarget = linkedList.getRoot();
+        for (int i = 0; i < index - 1; ++i) {
+            currentTarget = currentTarget.getNext();
+        }
+        currentTarget.setNext(currentTarget.getNext().getNext());
+        linkedList.setSize(linkedList.size() - 1);
+        return linkedList;
+    }
+
+    @Override
+    public ImmutableList set(int index, Object e) {
+        checkIndex(index);
+        ImmutableLinkedList linkedList = new ImmutableLinkedList(this);
+        Node currentTarget = linkedList.getRoot();
+        for (int i = 0; i < index; ++i) {
             currentTarget = currentTarget.getNext();
         }
         currentTarget.setValue(e);
@@ -60,33 +155,17 @@ public final class ImmutableLinkedList implements ImmutableList{
     }
 
     @Override
-    public ImmutableList addAll(Object[] c) {
-        return null;
-    }
-
-    @Override
-    public ImmutableList addAll(int index, Object[] c) {
-        return null;
-    }
-
-    @Override
-    public Object get(int index) {
-        return null;
-    }
-
-    @Override
-    public ImmutableList remove(int index) {
-        return null;
-    }
-
-    @Override
-    public ImmutableList set(int index, Object e) {
-        return null;
-    }
-
-    @Override
     public int indexOf(Object e) {
-        return 0;
+        Node currentTarget = getRoot();
+        int lookedIndex = -1;
+        for (int i = 0; i < size(); i++) {
+            if (currentTarget.getValue() == e) {
+                lookedIndex = i;
+                break;
+            }
+            currentTarget = currentTarget.getNext();
+        }
+        return lookedIndex;
     }
 
     @Override
@@ -96,16 +175,36 @@ public final class ImmutableLinkedList implements ImmutableList{
 
     @Override
     public ImmutableList clear() {
-        return null;
+        return new ImmutableLinkedList();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] ar = new Object[size()];
+        Node currentTarget = getRoot();
+        for (int i = 0; i < ar.length; ++i) {
+            ar[i] = currentTarget.getValue();
+            currentTarget = currentTarget.getNext();
+        }
+        return ar;
+    }
+
+    @Override
+    public String toString() {
+        String representation = "";
+        Node currentTarget = getRoot();
+        for (int i = 0; i < size(); i++) {
+            representation += currentTarget.getValue().toString();
+            currentTarget = currentTarget.getNext();
+            if (i != size - 1) {
+                representation += " ";
+            }
+        }
+        return representation;
     }
 }
--- src/test/java/ua/edu/ucu/collections/immutable/ImmutableLinkedListTest.java
+++ src/test/java/ua/edu/ucu/collections/immutable/ImmutableLinkedListTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ImmutableLinkedListTest {
-    
+
     @Test
     public void testAdd() {
         ImmutableLinkedList ill = new ImmutableLinkedList(25);
@@ -27,19 +27,151 @@ public class ImmutableLinkedListTest {
         assertEquals(-1, addedByIndexIll.getRoot().getNext().getValue());
     }
 
-    @Test
-    public void testAddByZeroIndex() {
-        ImmutableLinkedList ill = new ImmutableLinkedList(25);
-        ImmutableLinkedList addedIll = (ImmutableLinkedList) ill.add(10);
-        ImmutableLinkedList addedByIndexIll = (ImmutableLinkedList) addedIll.add(0, -1);
-        assertEquals(-1, addedByIndexIll.getRoot().getValue());
-    }
-
     @Test(expected = IndexOutOfBoundsException.class)
     public void testAddByIndexOutOfRange() {
         ImmutableLinkedList ill = new ImmutableLinkedList(25);
         ImmutableLinkedList addedIll = (ImmutableLinkedList) ill.add(10);
         ImmutableLinkedList addedByIndexIll = (ImmutableLinkedList) addedIll.add(10, -1);
         assertEquals(-1, addedByIndexIll.getRoot().getValue());
+    }
+
+    @Test
+    public void testAddAll() {
+        ImmutableLinkedList ill = new ImmutableLinkedList(25);
+        Object[] c = {26, 27, 28, 29};
+        ImmutableLinkedList addedIll = (ImmutableLinkedList) ill.addAll(c);
+        Node currentTarget = addedIll.getRoot();
+        for (int i = 0; i < 3; i++) {
+            currentTarget = currentTarget.getNext();
+        }
+        assertEquals(28, currentTarget.getValue());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testAddAllNone() {
+        ImmutableLinkedList ill = new ImmutableLinkedList(25);
+        Object[] c = {};
+        ImmutableLinkedList addedIll = (ImmutableLinkedList) ill.addAll(c);
+        Node currentTarget = addedIll.getRoot();
+        assertEquals(null, currentTarget.getNext().getValue());
+    }
+
+    @Test
+    public void testAddAllIndex() {
+        ImmutableLinkedList ill = new ImmutableLinkedList(25);
+        ImmutableLinkedList addedIll = (ImmutableLinkedList) ill.add(10);
+        Object[] c = {26, 27, 28, 29};
+        ImmutableLinkedList addedIllNew = (ImmutableLinkedList) addedIll.addAll(1, c);
+        Node currentTarget = addedIllNew.getRoot();
+        for (int i = 0; i < 5; i++) {
+            currentTarget = currentTarget.getNext();
+        }
+        assertEquals(10, currentTarget.getValue());
+    }
+
+    @Test
+    public void testGet() {
+        ImmutableLinkedList ill = new ImmutableLinkedList(25);
+        assertEquals(25, ill.get(0));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetIndexOutOfRange() {
+        ImmutableLinkedList ill = new ImmutableLinkedList(25);
+        assertEquals(1, ill.get(1230));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testRemoveOne() {
+        ImmutableLinkedList ill = new ImmutableLinkedList(25);
+        ImmutableLinkedList newll = (ImmutableLinkedList) ill.remove(0);
+        assertEquals(null, newll.getRoot());
+    }
+
+    @Test
+    public void testRemove() {
+        ImmutableLinkedList ill = new ImmutableLinkedList(25);
+        ImmutableLinkedList addedIll = (ImmutableLinkedList) ill.add(10);
+        Object[] c = {26, 27, 28, 29};
+        ImmutableLinkedList addedIllNew = (ImmutableLinkedList) addedIll.addAll(1, c);
+        ImmutableLinkedList newll = (ImmutableLinkedList) addedIllNew.remove(3);
+        Node currentTarget = newll.getRoot();
+        for (int i = 0; i < 3; i++) {
+            currentTarget = currentTarget.getNext();
+        }
+        assertEquals(29, currentTarget.getValue());
+    }
+
+    @Test
+    public void testSet() {
+        ImmutableLinkedList ill = new ImmutableLinkedList(25);
+        ImmutableLinkedList addedIll = (ImmutableLinkedList) ill.add(10);
+        Object[] c = {26, 27, 28, 29};
+        ImmutableLinkedList addedIllNew = (ImmutableLinkedList) addedIll.addAll(1, c);
+        ImmutableLinkedList newll = (ImmutableLinkedList) addedIllNew.set(3, 69);
+        Node currentTarget = newll.getRoot();
+        for (int i = 0; i < 3; i++) {
+            currentTarget = currentTarget.getNext();
+        }
+        assertEquals(69, currentTarget.getValue());
+    }
+
+    @Test
+    public void testIndexOf() {
+        ImmutableLinkedList ill = new ImmutableLinkedList(25);
+        ImmutableLinkedList addedIll = (ImmutableLinkedList) ill.add(10);
+        Object[] c = {26, 27, 28, 29};
+        ImmutableLinkedList addedIllNew = (ImmutableLinkedList) addedIll.addAll(1, c);
+        ImmutableLinkedList newll = (ImmutableLinkedList) addedIllNew.set(3, 69);
+        assertEquals(3, newll.indexOf(69));
+    }
+
+    @Test
+    public void testSize() {
+        ImmutableLinkedList ill = new ImmutableLinkedList(25);
+        ImmutableLinkedList addedIll = (ImmutableLinkedList) ill.add(10);
+        Object[] c = {26, 27, 28, 29};
+        ImmutableLinkedList addedIllNew = (ImmutableLinkedList) addedIll.addAll(1, c);
+        ImmutableLinkedList newll = (ImmutableLinkedList) addedIllNew.set(3, 69);
+        assertEquals(6, newll.size());
+    }
+
+    @Test
+    public void testClear() {
+        ImmutableLinkedList ill = new ImmutableLinkedList(25);
+        ImmutableLinkedList addedIll = (ImmutableLinkedList) ill.add(10);
+        Object[] c = {26, 27, 28, 29};
+        ImmutableLinkedList addedIllNew = (ImmutableLinkedList) addedIll.addAll(1, c);
+        ImmutableLinkedList newll = (ImmutableLinkedList) addedIllNew.clear();
+        assertEquals(0, newll.size());
+    }
+
+    @Test
+    public void testIsEmpty() {
+        ImmutableLinkedList ill = new ImmutableLinkedList(25);
+        ImmutableLinkedList addedIll = (ImmutableLinkedList) ill.add(10);
+        Object[] c = {26, 27, 28, 29};
+        ImmutableLinkedList addedIllNew = (ImmutableLinkedList) addedIll.addAll(1, c);
+        ImmutableLinkedList newll = (ImmutableLinkedList) addedIllNew.clear();
+        assertEquals(true, newll.isEmpty());
+    }
+
+    @Test
+    public void testToArray() {
+        ImmutableLinkedList ill = new ImmutableLinkedList(25);
+        ImmutableLinkedList addedIll = (ImmutableLinkedList) ill.add(10);
+        Object[] c = {26, 27, 28, 29};
+        ImmutableLinkedList addedIllNew = (ImmutableLinkedList) addedIll.addAll(1, c);
+        Object[] ar = {25, 26, 27, 28, 29, 10};
+        assertArrayEquals(ar, addedIllNew.toArray());
+    }
+
+    @Test
+    public void testToString() {
+        ImmutableLinkedList ill = new ImmutableLinkedList(25);
+        ImmutableLinkedList addedIll = (ImmutableLinkedList) ill.add(10);
+        Object[] c = {26, 27, 28, 29};
+        ImmutableLinkedList addedIllNew = (ImmutableLinkedList) addedIll.addAll(1, c);
+        assertEquals("25 26 27 28 29 10", addedIllNew.toString());
     }
 }
